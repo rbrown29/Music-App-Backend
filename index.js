460ba480f24f8491412759d1e8323c764b7f8295
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-04-11 00:39:11 -0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
+const corsAnywhere = require('cors-anywhere');
 const cors = require('cors');
 const morgan = require('morgan');
 
@@ -13,14 +14,14 @@ require('dotenv').config();
 const Port = process.env.PORT;
 
 const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(session({
@@ -30,9 +31,12 @@ app.use(session({
 }));
 
 app.use(cors(
-    { origin: 'http://localhost:3000, https://musicapp-3xgy.onrender.com/', credentials: true }
+    {
+        origin: ['http://localhost:3000, https://musicapp-3xgy.onrender.com/'],
+        credentials: true
+    }
 ));
-  
+
 app.use(morgan('dev'));
 
 const songsController = require('./controllers/songs.js');
@@ -49,6 +53,17 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong');
+});
+
+const corsProxy = corsAnywhere.createServer({
+    originWhitelist: ['http://localhost:3000', 'https://musicapp-3xgy.onrender.com/'], 
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+});
+
+app.use('/proxy', (req, res) => {
+    req.url = req.url.replace('/proxy/', '/');
+    corsProxy.emit('request', req, res);
 });
 
 app.listen(Port, () => {
