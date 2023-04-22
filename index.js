ad32a327b86dde2d9e8ad5cee19b1d42b95aea46
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-04-22 11:56:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const db = mongoose.connection;
 const cors = require('cors');
 app.use(cors());
 app.options('*', cors());
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 const morgan = require('morgan');
 
 require('dotenv').config();
